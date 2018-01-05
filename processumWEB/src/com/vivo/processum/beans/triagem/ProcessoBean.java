554c9c5cb9cf6d7baf656cf522b4e77365bc0f72
Janailson Oliveira <janailson.oliveira@gmail.com>
2018-01-05 19:08:27 -0200

    Commit Inicial

---
+++ processumWEB/src/com/vivo/processum/beans/triagem/ProcessoBean.java
@@ -0,0 +1,10 @@
+package com.vivo.processum.beans.triagem;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ProcessoBean {
+
+}
